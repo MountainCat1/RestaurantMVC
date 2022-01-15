6fd64ba5965070bc5ebc061d5e0c8a1a830ab6d6
MountainCat1 <72751263+MountainCat1@users.noreply.github.com>
2022-01-15 17:39:01 +0100

    Registration Model and Form

---
+++ RestaurantMVC/RestaurantMVC/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantMVC.Models;
+
+namespace RestaurantMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public IActionResult Register([FromForm] RegistrationDto registrationDto)
+        {
+            return Ok($"{registrationDto.Username}, {registrationDto.Email}, {registrationDto.Password}");
+        }
+
+        public IActionResult Login([FromForm] LoginDto registrationDto)
+        {
+            return Ok($"{registrationDto.Username}, {registrationDto.Email}, {registrationDto.Password}");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestaurantMVC/RestaurantMVC/Models/RegistrationDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace RestaurantMVC.Models
+{
+    public class RegistrationDto
+    {
+        [MinLength(3, ErrorMessage = "Username length must be between 3-32")]
+        [MaxLength(32, ErrorMessage = "Username length must be between 3-32")]
+        [Required]
+        //[StringLength(32, MinimumLength = 3,  ErrorMessage = "Username length must less than 32")]
+        public string Username { get; set; }
+
+        //[MinLength(1, ErrorMessage = "Name length must be between 1-32")]
+        //[MaxLength(32, ErrorMessage = "Name length must be between 1-32")]
+        [Required]
+        [EmailAddress(ErrorMessage ="UwU")]
+        public string Email { get; set; }
+
+        [MaxLength(32, ErrorMessage = "Password length must be between 1-32")]
+        [MinLength(6, ErrorMessage = "Password length must be between 6-32")]
+        [Required]
+        //[StringLength(32, MinimumLength = 3, ErrorMessage = "Password length must less than 32")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
